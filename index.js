6f369b336a0a3213fc88fc1722a2acf45af88730
Michael Li <dev.mike011235@gmail.com>
2020-07-10 17:39:27 -0400

    Added array of strings containing questions to take input for generating the README file.

---
+++ index.js
@@ -0,0 +1,23 @@
+const generateMarkdown = require("./generateMarkdown.js")
+
+const questions = [
+    "Give your README a badge.",
+    "What is the title of your project?",
+    "Describe your project.",
+    "List the table of contents. Hit enter with every entry.",
+    "What are the installation steps?",
+    "Describe how to use the app.",
+    "What is the license?",
+    "Who is contributing to this project?",
+    "How do you test the project?",
+    "QUESTIONS??"
+];
+
+function writeToFile(fileName, data) {
+}
+
+function init() {
+
+}
+
+init();
